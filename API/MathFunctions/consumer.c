7d84c2fd9ce43a66d008f6d25125e96940c1593e
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-27 22:59:59 -0500

    Documented both the squareRoot program and the consumer for an easier understanding

--- API/MathFunctions/consumer.c
+++ API/MathFunctions/consumer.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+/**
+ * Method: Consumes/calls the getSquareRoot.exe file
+ * to get the square root of a given number
+ * Parameter: Any positive number
+ */
 int main(int argc, char *argv[])
 {
+    /**
+     * Used system() function which alloes this program
+     * to run another program by passing a command line
+     * by string - Example(getSquareRoot.exe 30)
+     */
     return system(argv[1]);
 };
--- API/MathFunctions/squareRoot.c
+++ API/MathFunctions/squareRoot.c
@@ -2,8 +2,24 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * Method: Returns the square root of a given number
+ * Parameter: Any positive number
+ */
 int main(int argc, char *argv[])
 {
+    /**
+     * Declares a variable of type double and sets its value
+     * from the main parameter *argv[] 
+     * 
+     * Used atof() function from <stdlib.h> to parse the string
+     * given as parameter to its respective value as a double
+     */
     double number = atof(argv[1]);
+
+    /**
+     * Used sqrt() function from <math.h> to calculate the square root
+     * of the previously parsed number
+     */
     return sqrt(number);
 };
